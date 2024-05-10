9a6076bc3dd524e861b9d4abe2ef205604a7f00f
danielemalitesta <d.malitesta@gmail.com>
2024-05-10 19:16:05 +0200

    Start and end jobs.

--- DiffRec/submit_all.sh
+++ DiffRec/submit_all.sh
@@ -2,7 +2,7 @@
 
 first_script_id=$(sbatch ./scripts/$1/*__$2.sh | awk '{print $NF}')
 
-for i in {$2+1..$3}; do
+for i in {$($2+1)..$3}; do
     script="./scripts/$1/*__${i}.sh"
     # Submit the script with dependency on the previous script
     script_id=$(sbatch --dependency=afterok:${first_script_id} ${script} | awk '{print $NF}')
