339be24628477d45c042ff5806313d7d51c73b18
fabiodellaquila <dellaquila.fabio02@gmail.com>
2026-05-12 19:10:56 +0200

    Update main.py

--- GiffCF/main.py
+++ GiffCF/main.py
@@ -23,8 +23,8 @@ DEFAULT_DATA_PATHS = {
     'movielens': 'datasets/ml-1m_clean',
     'amazon': 'datasets/amazon-book_clean',
     'yelp': 'datasets/yelp_clean',
-    'ml1m': '/content/FairDiffRec/datasets/ml-1m',
-    'foursquare': '/content/FairDiffRec/datasets/foursquare_tky'
+    'ml-1m': '/content/FairDiffRec/datasets/ml-1m',
+    'foursquare_tky': '/content/FairDiffRec/datasets/foursquare_tky'
 }
 
 
