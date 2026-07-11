33365aa3a9d0f24d590be1b6e7947842fb3ccbe0
fabiodellaquila <dellaquila.fabio02@gmail.com>
2026-07-11 19:11:52 +0200

    Update run.sh

--- CDiff4Rec/run.sh
+++ CDiff4Rec/run.sh
@@ -12,16 +12,16 @@ case $dataset_name in
     "citeulike_t")
         cmd="python -u main.py --cuda --dataset citeulike_t --alpha 0.9 --beta 0.05 --gamma 0.05 --ur --r_agg avg --uf --no-tf --f_agg avg --lr 0.00005 --topk 20 --topk_pseudo_user 10000"
         ;;
-        "ml-1m")
+    "ml-1m")
         cmd="python -u main.py --cuda --dataset ml-1m --alpha 0.5 --r_agg avg --lr 0.0001 --topk 20 --steps 10 --weight_decay 0.0"
         ;;
-        "foursquare_tky")
+    "foursquare_tky")
         cmd="python -u main.py --cuda --dataset foursquare_tky --alpha 0.9 --r_agg sim --lr 0.0001 --topk 20 --steps 10 --weight_decay 0.0"
         ;;
-        "books")
+    "books")
         cmd="python -u main.py --cuda --dataset books --alpha 0.9 --r_agg sim --lr 0.0001 --topk 20 --steps 10 --weight_decay 0.0"
         ;;
-        "lastfm")
+    "lastfm")
         cmd="python -u main.py --cuda --dataset lastfm --alpha 0.7 --r_agg sim --lr 0.0001 --topk 20 --steps 10 --weight_decay 0.0"
         ;;
     *)
